c5520a25fbdcc579c4565b8c7c3a3446ac7c2656
awing256 <videomanoh@gmail.com>
2025-02-24 16:34:46 -0600

    basic search that hits lrclib.net api

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import QueryInput from "./components/Querying/QueryInput/QueryInput.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>OnGakkou</h1>
+          <QueryInput/>
+      </div>
+    </>
+  )
+}
+
+export default App
